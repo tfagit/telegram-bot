2214f7a73d97df5b92f4b72088f20a4accac08ef
Renan Guilherme <japareaggae@gmail.com>
2015-04-02 11:32:33 -0400

    Adicionar plugin 'pull'

---
+++ plugins/pull.lua
@@ -0,0 +1,17 @@
+do
+
+function run(msg, matches)
+	os.execute("cd ~/telegram-bot/; git pull")
+	return "O robô pode ter sido atualizado"
+end
+
+return {
+  description = "Atualiza a árvore git do bot",
+  usage = "!pull: atualiza a árvore git do bot",
+  patterns = {
+    "^[!#]pull$"
+  }, 
+  run = run 
+}
+
+end
