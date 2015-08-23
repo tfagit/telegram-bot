a4ac28d9e9972d32fbfe5ebd67a0de0180729ef6
Renan Guilherme <japareaggae@gmail.com>
2015-08-23 16:44:26 -0400

    binds.lua: Add three new triggers
    !assis, !cobretti, !sapequinha

--- plugins/binds.lua
+++ plugins/binds.lua
@@ -31,6 +31,18 @@ function get_kaponei()
   return binds_data.kaponei
 end
 
+function get_assis(){
+  return binds_data.assis
+}
+
+function get_cobretti(){
+  return binds_data.cobretti
+}
+
+function get_sapequinha(){
+  return binds_data.sapequinha
+}
+
 function get_bind()
   return (binds_data.binds[math.random(#(binds_data.binds))])
 end
@@ -52,6 +64,18 @@ function run(msg, matches)
     return get_kaponei()
   end
   
+  if matches[1] == "assis" then
+    return get_assis()
+  end
+  
+  if matches[1] == "cobretti" then
+    return get_cobretti()
+  end
+  
+  if matches[1] == "sapequinha" then
+    return get_sapequinha()
+  end
+  
   if matches[1] == "bind" then
     return get_bind()
   end
@@ -64,6 +88,9 @@ return {
     "!esquilo",
     "!vacilao",
     "!kaponei",
+	"!assis",
+    "!cobretti",
+    "!sapequinha",
     "!bind: Retorna um bind aleatório"
   },
   patterns = {
@@ -71,6 +98,9 @@ return {
     "^[!#](esquilo)$",
     "^[!#](vacilao)$",
     "^[!#](kaponei)$",
+	"^[!#](assis)$",
+    "^[!#](cobretti)$",
+    "^[!#](sapequinha)$",
     "^[!#](bind)s?$"
   },
   run = run
