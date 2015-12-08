49d282adbad498f9803fda0e431ba07df062116f
Renan Guilherme <japareaggae@gmail.com>
2015-12-08 11:43:49 -0400

    version: Fix whitespace in message

--- plugins/version.lua
+++ plugins/version.lua
@@ -1,9 +1,9 @@
 do
 
 function run(msg, matches)
-  return 'telegram-bot v'.. VERSION .. [[ -tfa
-  Código-fonte: https://github.com/tfagit/telegram-bot
-  Este programa está disponível sob a licença GNU GPL v2.]]
+  return 'telegram-bot v'.. VERSION .. [[-tfa
+Código-fonte: https://github.com/tfagit/telegram-bot
+Este programa está disponível sob a licença GNU GPL v2.]]
 end
 
 return {
